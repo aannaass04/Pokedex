46ab8500c14f6a8facf091f44e50108813205d4b
Anas Bellahcene <anas@mail.com>
2026-04-14 21:03:21 +0200

    Dia 4 haciendo la pokedex

---
+++ src/main/java/es/etg/prog/pokedex/model/entities/Pokedex.java
@@ -0,0 +1,15 @@
+package es.etg.prog.pokedex.model.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import java.util.ArrayList;
+import java.util.List;
+
+import es.etg.prog.pokedex.model.persistence.IPokemonRepository;
+
+@Data @AllArgsConstructor
+public class Pokedex {
+ 
+    private IPokemonRepository repository;
+    private List<Pokemon> pokemons = new ArrayList<>();
+}
---
+++ src/main/java/es/etg/prog/pokedex/model/entities/Pokemon.java
@@ -0,0 +1,11 @@
+package es.etg.prog.pokedex.model.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data @AllArgsConstructor
+public class Pokemon {
+    private final int id;
+    private String nombre;
+    private String tipo;
+}
